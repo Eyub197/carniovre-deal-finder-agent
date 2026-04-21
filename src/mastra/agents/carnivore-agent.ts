f91fa82b3ac84f4edca65fc2e312f6ce8b2aa4e7
Eyub197 <eyubmehmed122@gmail.com>
2026-04-21 22:08:49 +0300

    Getting in there. Now it can open a borwser and I even got 2 screenshots soon it will be better

--- src/mastra/agents/carnivore-agent.ts
+++ src/mastra/agents/carnivore-agent.ts
@@ -1,11 +1,27 @@
+import { AgentBrowser } from "@mastra/agent-browser";
 import { Agent } from "@mastra/core/agent";
-import { Memory } from "@mastra/memory";
-import { weatherTool } from "../tools/weather-tool";
-import { scorers } from "../scorers/weather-scorer";
+import { Workspace, LocalFilesystem } from "@mastra/core/workspace";
+
+if (!process.env.SCREENSHOT_PATH) {
+	throw new Error("SCREENSHOT_PATH environment variable is not set");
+}
+
+export const workspace = new Workspace({
+	filesystem: new LocalFilesystem({
+		basePath: process.env.SCREENSHOT_PATH,
+	}),
+});
+
+export const browser = new AgentBrowser({
+	headless: false,
+});
 
 export const carnivoreAgent = new Agent({
 	id: "carnivore-agent",
 	name: "Carnivore agent",
 	instructions: `You are helpful deal finder, you goal is to find deals for carnivore diet people. Aka like eggs on discount and meats`,
 	model: "openai/gpt-5-mini",
+	browser,
+	workspace,
 });
+// Instrutciontns to be added when someome tells you open a webPage use the browser goto tool and then other like that to
--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,5 +1,5 @@
 import { Mastra } from "@mastra/core/mastra";
-import { carnivoreAgent } from "./agents/weather-agent";
+import { carnivoreAgent } from "./agents/carnivore-agent";
 
 export const mastra = new Mastra({
 	agents: { carnivoreAgent },
